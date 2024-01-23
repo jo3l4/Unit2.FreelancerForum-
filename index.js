ab653cdabcf700b9a6422891dee039a586aaa5ea
joel c <150722950+jo3l4@users.noreply.github.com>
2024-01-22 23:53:18 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+let freeLance=[
+    {
+        name: "Alice",
+        occupation: "Writer",
+        startingPrice: 30
+    },
+    {
+        name: "Bob",
+        occupation: "teacher",
+        startingPrice: 50
+    },
+    {
+        name: "Carol",
+        occupation: "Programmer",
+        startingPrice: 70
+    }
+]
+console.log(freeLance)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
